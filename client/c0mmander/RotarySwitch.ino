bb440fdcbf0931cba5562b7551f21978194f8f4d
Alex Coulcher <alex@polyglot.rodeo>
2021-04-02 11:43:09 +0100

    updates

--- client/c0mmander/RotarySwitch.ino
+++ client/c0mmander/RotarySwitch.ino
@@ -4,21 +4,23 @@
 class RotarySwitch {
   private:
     int _channels[CHANNEL_COUNT][2] = { 
-      { 0, 2999 },    // 0
-      { 2999, 3149 }, // 1
-      { 3149, 3259 }, // 2
-      { 3259, 3499 }, // 3
-      { 3499, 3650 }, // 4
-      { 3650, 3710 }, // 5
-      { 3710, 3799 }, // 6
-      { 3799, 3890 }, // 7
-      { 3890, 3980 }, // 8
-      { 3980, 4020 }, // 9
-      { 4020, 4075 }, // 10
-      { 4075, 5000 }  // 11
+      { 0, 3200 },    // 0
+      { 3200, 3400 }, // 1
+      { 3400, 3600 }, // 2
+      { 3600, 3700 }, // 3
+      { 3700, 3800 }, // 4
+      { 3800, 3900 }, // 5
+      { 3900, 4000 }, // 6
+      { 4000, 4095 }, // 7
+      { 4095, 4100 }, // 8
+      
+      { 4100, 4020 }, // 9 No resitor, the range will never get this high.
+      { 4200, 4075 }, // 10
+      { 4300, 5000 }  // 11
     };
     
     int _pin;
+    int _currAnalogVal;
     int _readings[READING_COUNT];
     int _lastAvgVal;
     int _value;
@@ -34,10 +36,12 @@ class RotarySwitch {
         total += _readings[i];
       }
       
-      int currAnalogVal = analogRead(_pin);
+      _currAnalogVal = analogRead(_pin);
 
-      _readings[READING_COUNT - 1] = currAnalogVal;
-      total += currAnalogVal;
+//      Serial.println(_currAnalogVal);
+
+      _readings[READING_COUNT - 1] = _currAnalogVal;
+      total += _currAnalogVal;
 
       return floor(total / READING_COUNT);
     }
--- client/c0mmander/c0mmander.ino
+++ client/c0mmander/c0mmander.ino
@@ -1,5 +1,7 @@
 // https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
 
+// add pir to turn on
+
 #define COMMAND_COUNT 12
 
 #define ROTARY_PIN 33
